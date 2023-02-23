7751119f8523b622a52eff7f98da67921e5dfed7
pavan-459 <pavan459@protonmail.ch>
2023-02-23 15:40:46 +0530

    a

---
+++ server.js
@@ -0,0 +1,36 @@
+// require('dotenv').config()
+
+// const express = require('express')
+// const app = express()
+// const mongoose = require('mongoose')
+// mongoose.set('strictQuery', false);
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open',()=> console.log('Connected to Database'))
+
+// app.use(express.json())
+
+// const subscribersRouter = require('./routes/subscribers')
+// app.use('/subscribers', subscribersRouter)
+
+// app.listen(3000, () => console.log('Server Started'))
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+app.listen(3000, () => console.log('Server Started'))
+mongoose.set('strictQuery',true)
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
